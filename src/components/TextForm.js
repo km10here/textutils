4c7a028c3fb7c882344471b46e20033c9e6a1600
km10here <kartavya.magoo@gmail.com>
2022-02-19 13:53:59 +0530

    added convert to lowercase, word count and preview

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,16 +3,24 @@ export default function TextForm(props) {
     const [text, setText] = useState();
     
     const handleUpclick=()=>{
-        console.log("Uppercase was clicked" +text);
+        // console.log("Uppercase was clicked" +text);
         let newtext = text.toUpperCase();
         setText(newtext)
     }
+
+    const handleLowclick=()=>{
+      // console.log("Lowercase was clicked" +text);
+      let newtext = text.toLowerCase();
+      setText(newtext)
+    }
+
     const handleOnChange=(event)=>{
-        console.log("on change");
+        // console.log("on change");
         setText(event.target.value);
     }
     return(
-      <div>
+      <>
+       <div>
            <div className="form-group">
       <h1>{props.heading}</h1>
     <label htmlFor="mybox">Text Area</label>
@@ -20,7 +28,19 @@ export default function TextForm(props) {
   </div>
 
   <button type="button" className="btn btn-primary" onClick={handleUpclick}>Convert to Upper Case</button>
+  <button type="button" className="btn btn-success mx-3" onClick={handleLowclick}>Convert to Lower Case</button>
+
       </div>
-      
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").length * 0.008} Minuits to read </p>
+        <h2>Preview</h2>
+        <p>
+          {text}
+        </p>
+      </div>
+      </>
+     
   )
 }
